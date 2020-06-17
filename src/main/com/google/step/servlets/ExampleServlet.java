6702c62afd7dfc818dea112c710c6c2a64119699
alexkimt <alexkimt@gmail.com>
2020-06-17 17:36:34 -0400

    Add placeholder java files to set up project file structure.

---
+++ src/main/com/google/step/servlets/ExampleServlet.java
@@ -0,0 +1,39 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.step.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+
+/** Servlet that handles requests to the blog page */
+@WebServlet("/example")
+public class ExampleServlet extends HttpServlet {
+
+  @Override
+  public void init(){
+    //This is the init method!
+  }
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    //This is the get method!
+  }
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    //This is the post method!
+  }
+}
