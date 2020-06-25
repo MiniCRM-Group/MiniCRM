7a58aa13cf39cda16a73610ad41ae92ea6b240d4
Roddy Taipe <roddy@google.com>
2020-06-25 20:14:42 +0000

    Refactor features

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
 
+interface Feature {
+  icon: string,
+  name: string,
+  description: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  features: Feature[] = [
+    {
+      icon: "list_all",
+      name: "Lead Forms Compatible",
+      description: ""
+    },
+    {
+      icon: "table_chart",
+      name: "Qualify Leads",
+      description: ""
+    },
+    {
+      icon: "email",
+      name: "Contact Leads",
+      description: ""
+    },
+    {
+      icon: "bar_chart",
+      name: "Analyze Leads",
+      description: ""
+    }
+  ];
 }
