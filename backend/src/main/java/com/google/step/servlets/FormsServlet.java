98eedc0ae2e99143cf49c832dc13d6dc589368b5
FluffyPenguin <alexkimt@gmail.com>
2020-06-29 19:15:50 +0000

    Add support for application/json content body. Now stores Form entities in datastore and checks whether they exist before returning a new googlekey.

--- backend/src/main/java/com/google/step/servlets/FormsServlet.java
+++ backend/src/main/java/com/google/step/servlets/FormsServlet.java
@@ -14,10 +14,17 @@
 
 package com.google.step.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.users.User;
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.step.data.Form;
 import com.google.step.utils.AdvertiserUtil;
 import com.google.step.utils.UserAuthenticationUtil;
 
@@ -27,6 +34,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.security.SecureRandom;
+import java.util.Map;
 import java.util.Random;
 
 @WebServlet("/api/forms")
@@ -49,11 +57,37 @@ public class FormsServlet extends HttpServlet {
         String webhookUrl = generateUserWebhook(request, UserAuthenticationUtil.getCurrentUser());
 
         response.setContentType("application/json;");
-        response.getWriter().println(new WebhookResponse(webhookUrl, "").toJson());
+        response.getWriter().println(new WebhookResponse(webhookUrl, "", 0).toJson());
     }
 
     /**
      * Returns JSON representing the advertiser's webhook and google key
+     * Request body needs to contain form_id and form_name in application/x-www-form-urlencoded or application/json
+     * Example POST request using application/x-www-form-urlencoded:
+     * fetch('http://localhost:8080/api/forms', {
+     *     method: 'POST',
+     *     body: params,
+     * })
+     * .then(res => res.json())
+     * .then(console.log)
+     *
+     *  where params is a URLSearchParams object with keys form_id and form_name.
+     *  URLSearchParams automatically sets encoding to application/x-www-form-urlencoded.
+     *  Example POST request using application/json:
+     *  fetch('http://localhost:8080/api/forms', {
+     *   method: 'POST',
+     *   body: JSON.stringify({
+     *     form_id: "1234",
+     *     form_name: "exampleForm"
+     *   }),
+     *   headers: {
+     *     'Content-type': 'application/json; charset=UTF-8'
+     *   }
+     * })
+     * .then(res => res.json())
+     * .then(console.log)
+     *
+     * Note: form_id should be a string not a number without quotation marks.
      * @param request       the HTTP Request. Expecting parameter form_id with the form_id to add
      * @param response      the HTTP Response
      * @throws IOException  if an input exception occurs with the response writer or reader
@@ -64,6 +98,35 @@ public class FormsServlet extends HttpServlet {
             response.sendRedirect("/");
             return;
         }
+        //TODO: Change to JSON instead of urlencoding
+        long formId;
+        String formName;
+        if (request.getContentType().contains("application/x-www-form-urlencoded")) {
+            System.out.println("form-urlencoding!");
+            formId = Long.parseLong(request.getParameter("form_id"));
+            formName = request.getParameter("form_name");
+        } else if (request.getContentType().contains("application/json")) {
+            System.out.println("JSON!");
+            Gson gson = new Gson();
+            Map<String, String> jsonMap = gson.fromJson(request.getReader(), Map.class);
+            formId = Long.parseLong(jsonMap.get("form_id"));
+            formName = jsonMap.get("form_name");
+        } else {
+            response.sendError(415,
+                    "Content type not supported. Try application/x-www-form-urlencoded or application/json.");
+            return;
+        }
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Query query = new Query("Form")
+                .setFilter(new Query.FilterPredicate("formId", Query.FilterOperator.EQUAL, formId))
+                .setKeysOnly();
+        PreparedQuery queryResults = datastore.prepare(query);
+        if (queryResults.asList(FetchOptions.Builder.withDefaults()).size() > 0) { //there already exists a form with this id
+            response.sendError(403, "Form ID already claimed by anoter user.");
+            //TODO: make sure this form is verified to send back this response
+        }
+
 
         String webhookUrl = generateUserWebhook(request, UserAuthenticationUtil.getCurrentUser());
         //generate random google key (currently 20 chars)
@@ -72,8 +135,17 @@ public class FormsServlet extends HttpServlet {
         for (int i = 0; i < 20; i++) {
             googleKeyBuilder.append(alphanumerics[rand.nextInt(alphanumerics.length)]);
         }
+        String googleKey = googleKeyBuilder.toString();
+
+        //store the form entity in the datastore
+        Form newForm = new Form(formId,
+                formName,
+                AdvertiserUtil.createAdvertiserKey(UserAuthenticationUtil.getCurrentUser()),
+                googleKey);
+        datastore.put(newForm.asEntity());
+
         response.setContentType("application/json;");
-        response.getWriter().println(new WebhookResponse(webhookUrl, googleKeyBuilder.toString()).toJson());
+        response.getWriter().println(new WebhookResponse(webhookUrl, googleKey, formId).toJson());
     }
 
     /**
@@ -103,13 +175,20 @@ public class FormsServlet extends HttpServlet {
         private String googleKey;
 
         /**
+         * The id of the form
+         */
+        private long formId;
+
+        /**
          * Constructor for response to send back to user containing the webhook and google key
          * @param webhookUrl url to receive lead data from Google Ads
          * @param googleKey  randomly generated google key string
+         * @param formId     id of the form
          */
-        WebhookResponse(String webhookUrl, String googleKey) {
+        WebhookResponse(String webhookUrl, String googleKey, long formId) {
             this.webhookUrl = webhookUrl;
             this.googleKey = googleKey;
+            this.formId = formId;
         }
 
         public String toJson(){
