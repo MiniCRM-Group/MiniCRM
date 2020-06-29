ed9b2423cfd453cd0ca1fe2073520cd4216637f6
Amanuel Taddesse <atesfaye@google.com>
2020-06-29 22:26:44 +0000

    fix nits

--- frontend/src/app/leads/model/lead.model.ts
+++ frontend/src/app/leads/model/lead.model.ts
@@ -2,13 +2,13 @@ import { ColumnData } from './column-data.model';
 
 // An interface to handle fetching a lead's property
 export interface Lead {
-   date:Date;
-   lead_id:string;
-   campaign_id:number;
-   gcl_id:string;
-   api_version:string;
-   form_id:number;
-   google_Key:string;
+   date : Date;
+   lead_id : string;
+   campaign_id : number;
+   gcl_id : string;
+   api_version : string;
+   form_id : number;
+   google_Key : string;
    column_data : ColumnData;
    is_test : boolean;
    adgroup_id : number;
