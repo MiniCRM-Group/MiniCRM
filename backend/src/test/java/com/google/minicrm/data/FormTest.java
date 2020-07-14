62c0cca6f586e6fc77cdd1a23384d2baa0bb8596
FluffyPenguin <alexkimt@gmail.com>
2020-07-14 23:45:58 +0000

    Correctly setup testing API environment

--- backend/src/test/java/com/google/minicrm/data/FormTest.java
+++ backend/src/test/java/com/google/minicrm/data/FormTest.java
@@ -24,6 +24,7 @@ import com.google.appengine.api.users.User;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import com.google.appengine.tools.development.testing.LocalUserServiceTestConfig;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -41,7 +42,12 @@ public final class FormTest {
 
   @Before
   public void setUp() {
+    helper.setUp();
+  }
 
+  @After
+  public void tearDown(){
+    helper.tearDown();
   }
 
   @Test
