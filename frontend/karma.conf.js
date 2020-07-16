edf9dd1906deccc18061938e0ce3dea2a0eaf1cb
Roddy Taipe <roddy@google.com>
2020-07-16 14:54:50 +0000

    Add puppeteer

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,6 +1,10 @@
+// setting CHROME_BIN env variable for CI
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+process.env.CHROME_BIN = require('puppeteer').executablePath()
 module.exports = function (config) {
   config.set({
     basePath: '',
