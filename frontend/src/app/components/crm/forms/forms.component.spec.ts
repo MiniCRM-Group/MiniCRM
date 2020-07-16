b5c3630f278f4c3d851b9b6ee7ed4f4254670d90
Roddy Taipe <roddy@google.com>
2020-07-16 21:37:22 +0000

    Fixed all tests

--- frontend/src/app/components/crm/forms/forms.component.spec.ts
+++ frontend/src/app/components/crm/forms/forms.component.spec.ts
@@ -6,6 +6,8 @@ import { FormService } from 'src/app/services/form.service';
 import { of } from 'rxjs';
 import { FormsResponse, Form } from 'src/app/models/server_responses/forms-response.model';
 import { HttpClientModule } from '@angular/common/http';
+import { WebhookService } from 'src/app/services/webhook.service';
+import { WebHookResponse } from 'src/app/models/server_responses/webhook-response.model';
 
 describe('FormsComponent', () => {
   let component: FormsComponent;
@@ -13,13 +15,19 @@ describe('FormsComponent', () => {
     getForms: () => of<FormsResponse>({ forms: [] }),
     unlinkForms: (req: Form[]) => 'ok'
   };
+  const webhookService: Partial<WebhookService> = {
+    getWebhook: () => of<WebHookResponse>({ webhookUrl: '/', googleKey: 'abc' })
+  }
   let fixture: ComponentFixture<FormsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ FormsComponent ],
       imports: [ MatDialogModule, HttpClientModule ],
-      providers: [ { provide: FormService, useValue: formService } ]
+      providers: [ 
+        { provide: FormService, useValue: formService },
+        { provide: WebhookService, useValue: webhookService }
+      ]
     })
     .compileComponents();
   }));
