4e0265a9f5bc8559f25a2f64c05e2f120c1d523f
FluffyPenguin <alexkimt@gmail.com>
2020-07-17 23:09:47 +0000

    Make campaigns table show with crm-table

--- frontend/src/app/components/crm/campaigns/campaigns.component.ts
+++ frontend/src/app/components/crm/campaigns/campaigns.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
 import { Campaign } from 'src/app/models/server_responses/campaign.model';
 import { CampaignService } from 'src/app/services/campaign.service';
+import { CrmTableComponent } from '../../shared/crm-table/crm-table.component';
 
 @Component({
   selector: 'app-campaigns',
@@ -10,7 +11,9 @@ import { CampaignService } from 'src/app/services/campaign.service';
   styleUrls: ['./campaigns.component.css']
 })
 export class CampaignsComponent implements OnInit {
+  @ViewChild('campaignsCrmTable') campaignsTable : CrmTableComponent<Campaign>;
   campaigns: Observable<Campaign[]> = this.campaignService.getAllCampaigns();
+  keyOrdering: string[] = ['campaignId', 'campaignName', 'date'];
   
   constructor(private campaignService : CampaignService, 
     private titleService: Title) {
