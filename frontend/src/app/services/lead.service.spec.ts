fc8b1478a3bf3ccb0b4a78b5596b4ad447103128
ataddesse <amanueltesfaye44@gmail.com>
2020-07-20 17:42:23 +0000

    finalize lead service test file

--- frontend/src/app/services/lead.service.spec.ts
+++ frontend/src/app/services/lead.service.spec.ts
@@ -1,18 +1,22 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { LeadService } from './lead.service';
-import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { Lead, LeadsResponse } from '../models/server_responses/lead.model';
+import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 
 describe('LeadService', () => {
-  let service: LeadService;
+  let leadService: LeadService;
   let httpMock: HttpTestingController;
-
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [ HttpClientTestingModule ],
       providers: [ LeadService ]
     });
-    service = TestBed.inject(LeadService);
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    leadService = TestBed.inject(LeadService);
     httpMock = TestBed.inject(HttpTestingController);
   });
 
@@ -21,6 +25,88 @@ describe('LeadService', () => {
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(leadService).toBeTruthy();
   });
+
+  it('should return an Observable<FormResponse>', () => {
+    const expectedLeads: LeadsResponse = {
+      leads: [
+    { date: new Date(), leadId: 'cKlwe8W4xN4CFcoFrwkdyHEByt', campaignId: 204256,
+      gclId: 'uebai8W4xN4CFcoFrwkdyHEByw', apiVersion: '1.0', formId: 9574,
+      googleKey: 'hWDxdHeMT1yq9DZgt8Io',
+      columnData: {FULL_NAME: 'kali Ali', EMAIL: 'kali@gmail.com', PHONE_NUMBER: '+15334567890'},
+      isTest: false, adgroupId : 0, creativeId: 0, status: 'OPEN', notes: ''},
+    { date: new Date(), leadId: 'dKlwe8W4xN4CFcoFrwkdyHEByt', campaignId: 204256,
+      gclId: 'uebai8W4xN4CFcoFrwkdyHEByw', apiVersion: '1.0', formId: 9574,
+      googleKey: 'hWDxdHeMT1yq9DZgt8Io',
+      columnData: {FULL_NAME: 'lali Ali', EMAIL: 'lali@gmail.com', PHONE_NUMBER: '+25334567890'},
+      isTest: false, adgroupId : 0, creativeId: 0, status: 'OPEN', notes: ''}
+      ] as Lead[]
+    };
+
+    leadService.getAllLeads().subscribe((res: Lead[]) => {
+      expect(res).toEqual(expectedLeads.leads);
+    });
+
+    const req = httpMock.expectOne(leadService.url);
+    expect(req.request.method).toBe('GET');
+    req.flush(expectedLeads);
+  });
+
+  it('should be OK returning no leads', () => {
+      const expectedLeads: LeadsResponse = {
+      leads: [
+          // empty Observable
+      ] as Lead[]
+    };
+      leadService.getAllLeads().subscribe(
+        leads => expect(expectedLeads.leads.length).toEqual(0, 'should have empty leads array'),
+        fail
+      );
+
+      const req = httpTestingController.expectOne(leadService.url);
+      req.flush([]); // Respond with no heroes
+  });
+
+
+  it('should return expected leads (called multiple times)', () => {
+    const expectedLeads: LeadsResponse = {
+      leads: [
+            { date: new Date(), leadId: 'cKlwe8W4xN4CFcoFrwkdyHEByt', campaignId: 204256,
+              gclId: 'uebai8W4xN4CFcoFrwkdyHEByw', apiVersion: '1.0', formId: 9574,
+              googleKey: 'hWDxdHeMT1yq9DZgt8Io',
+              columnData: {FULL_NAME: 'kali Ali', EMAIL: 'kali@gmail.com', PHONE_NUMBER: '+15334567890'},
+              isTest: false, adgroupId : 0, creativeId: 0, status: 'OPEN', notes: ''},
+            { date: new Date(), leadId: 'dKlwe8W4xN4CFcoFrwkdyHEByt', campaignId: 204256,
+              gclId: 'uebai8W4xN4CFcoFrwkdyHEByw', apiVersion: '1.0', formId: 9574,
+              googleKey: 'hWDxdHeMT1yq9DZgt8Io',
+              columnData: {FULL_NAME: 'lali Ali', EMAIL: 'lali@gmail.com', PHONE_NUMBER: '+25334567890'},
+              isTest: false, adgroupId : 0, creativeId: 0, status: 'OPEN', notes: ''},
+            { date: new Date(), leadId: 'fKlwe8W4xN4CFcoFrwkdyHEByt', campaignId: 204256,
+              gclId: 'uebai8W4xN4CFcoFrwkdyHEByw', apiVersion: '1.0', formId: 9574,
+              googleKey: 'hWDxdHeMT1yq9DZgt8Io',
+              columnData: {FULL_NAME: 'Yali Ali', EMAIL: 'yali@gmail.com', PHONE_NUMBER: '+45334567890'},
+              isTest: false, adgroupId : 0, creativeId: 0, status: 'OPEN', notes: ''}
+      ] as Lead[]
+  };
+    leadService.getAllLeads().subscribe();
+    leadService.getAllLeads().subscribe();
+    leadService.getAllLeads().subscribe(
+      leads => expect(leads).toEqual(expectedLeads.leads, 'should return expected heroes'),
+      fail
+    );
+
+    const requests = httpTestingController.match(leadService.url);
+    expect(requests.length).toEqual(3, 'calls to getHeroes()');
+
+    // Respond to each request with different mock hero results
+    requests[0].flush([]);
+    requests[1].flush([{date: new Date(), leadId: 'dKlwe8W4xN4CFcoFrwkdyHEByt', campaignId: 204256,
+                                      gclId: 'uebai8W4xN4CFcoFrwkdyHEByw', apiVersion: '1.0', formId: 9574,
+                                      googleKey: 'hWDxdHeMT1yq9DZgt8Io',
+                                      columnData: {FULL_NAME: 'lali Ali', EMAIL: 'lali@gmail.com', PHONE_NUMBER: '+25334567890'},
+                                      isTest: false, adgroupId : 0, creativeId: 0, status: 'OPEN', notes: ''}]);
+    requests[2].flush(expectedLeads);
+  });
+
 });
