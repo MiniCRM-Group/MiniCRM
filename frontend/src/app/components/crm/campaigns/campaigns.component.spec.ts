8281889c4c6b37bd1378cc1dbb11fdf66afcafc1
FluffyPenguin <alexkimt@gmail.com>
2020-07-22 01:12:18 +0000

    Fix merge issues in campaigns.component.spec

--- frontend/src/app/components/crm/campaigns/campaigns.component.spec.ts
+++ frontend/src/app/components/crm/campaigns/campaigns.component.spec.ts
@@ -1,8 +1,12 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CampaignsComponent } from './campaigns.component';
 import { CampaignsModule } from './campaigns.module';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { CampaignService } from 'src/app/services/campaign.service';
+import { of } from 'rxjs';
+import { Campaign } from 'src/app/models/server_responses/campaign.model';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 
 describe('CampaignsComponent', () => {
   const campaignService: Partial<CampaignService> = {
@@ -13,6 +17,10 @@ describe('CampaignsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      declarations: [ CampaignsComponent ],
+      providers: [
+        { provide: CampaignService, useValue: campaignService }
+      ],
       imports: [ CampaignsModule, NoopAnimationsModule],
     })
     .compileComponents();
