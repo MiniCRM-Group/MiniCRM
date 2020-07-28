8feb4aff14450eb2cb4ce170dbe5f9837fed1bf1
FluffyPenguin <alexkimt@gmail.com>
2020-07-28 22:39:40 +0000

    Add notes field and condition update button to lead-details dialog

--- frontend/src/app/components/crm/leads/lead-details/lead-details.component.ts
+++ frontend/src/app/components/crm/leads/lead-details/lead-details.component.ts
@@ -4,15 +4,22 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
   selector: 'app-lead-details',
   templateUrl: 'lead-details.component.html',
+  styleUrls: ['./lead-details.component.css']
 })
 export class LeadDetailsComponent {
-
+  private initialNotes: string;
+  updated: boolean = false;
   constructor(
     public dialogRef: MatDialogRef<LeadDetailsComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) { }
+    @Inject(MAT_DIALOG_DATA) public data: any) {
+      this.initialNotes = data.lead.notes;
+     }
 
   onNoClick(): void {
     this.dialogRef.close();
   }
 
+  notesUpdated() {
+    this.updated = (this.initialNotes !== this.data.lead.notes);
+  }
 }
