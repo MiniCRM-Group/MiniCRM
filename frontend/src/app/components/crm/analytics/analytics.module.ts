a6f9733419286ab8d48b7383e3ba593d5f9886bb
FluffyPenguin <alexkimt@gmail.com>
2020-08-04 20:04:37 +0000

    Remove unused import in analytics.module.ts

--- frontend/src/app/components/crm/analytics/analytics.module.ts
+++ frontend/src/app/components/crm/analytics/analytics.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnalyticsComponent } from './analytics.component';
 import { MapComponent } from './map/map.component';
-import { ChartsComponent } from './charts/charts.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { ChartsModule } from './charts/charts.module';
