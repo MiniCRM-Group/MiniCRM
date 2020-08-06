ff13e4d85aa3cbdab08c7a60140d922149ace077
FluffyPenguin <alexkimt@gmail.com>
2020-08-06 00:22:00 +0000

    Add other chart types

--- frontend/src/app/components/crm/analytics/charts/charts.component.ts
+++ frontend/src/app/components/crm/analytics/charts/charts.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { LeadService } from 'src/app/services/lead.service';
 import { Lead, LeadStatus } from 'src/app/models/server_responses/lead.model';
 import { HumanizeEnumPipe } from 'src/app/pipes/humanize-enum.pipe';
+import { CampaignService } from 'src/app/services/campaign.service';
+import { FormService } from 'src/app/services/form.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-charts',
@@ -11,34 +14,45 @@ import { HumanizeEnumPipe } from 'src/app/pipes/humanize-enum.pipe';
 export class ChartsComponent implements OnInit {
   pipe: HumanizeEnumPipe = new HumanizeEnumPipe();
   leads: Lead[];
-  leadStatuses: object[];
+  leadsByStatus: object[];
+  leadsByForm: object[];
+  leadsByCampaign: object[];
   view: any[] = [700, 400];
   colorScheme = {
     domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
   };
   isLoading: boolean;
+  /**
+   * Maps formId to formName
+   */
+  formNameMap: Map<number, string>;
+  /**
+   * Maps campaignId to campaignName
+   */
+  campaignNameMap: Map<number, string>;
 
-  constructor(private leadService: LeadService) { 
-    this.loadAllLeads();
+  constructor(private readonly leadService: LeadService,
+              private readonly formService: FormService,
+              private readonly campaignService: CampaignService) {
   }
 
   ngOnInit(): void {
-  }
-
-  /**
-   * This will access the leads from the leadService and handle the filterPredicate and the isLoading boolean value.
-   */
-  loadAllLeads(): void {
     this.isLoading = true;
-    this.leadService.getAllLeads().subscribe((leads) => {
-      this.leads = leads;
+    forkJoin([
+      this.formService.getFormNameMap(),
+      this.campaignService.getCampaignNameMap(),
+      this.leadService.getAllLeads()
+    ]).subscribe(t => {
+      this.formNameMap = t[0];
+      this.campaignNameMap = t[1];
+      this.leads = t[2];
       this.generateData();
       this.isLoading = false;
     });
   }
-
+  
   generateData(): void {
-    // fill out the leadStatuses object
+    // fill out the leadsByStatus object
     const statusMap: Map<LeadStatus, number> = new Map<LeadStatus, number>();
     for (const lead of this.leads) {
       if (statusMap.has(lead.status)) {
@@ -47,13 +61,49 @@ export class ChartsComponent implements OnInit {
         statusMap.set(lead.status, 1);
       }
     }
-    this.leadStatuses = [];
+    this.leadsByStatus = [];
     statusMap.forEach((count: number, key: LeadStatus) => {
       const dataPoint = {
         name: this.pipe.transform(LeadStatus[key]),
         value: count
       };
-      this.leadStatuses.push(dataPoint);
+      this.leadsByStatus.push(dataPoint);
+    });
+
+    // fill out the leadsByForm object
+    const formMap: Map<number, number> = new Map<number, number>();
+    for (const lead of this.leads) {
+      if (formMap.has(lead.formId)) {
+        formMap.set(lead.formId, formMap.get(lead.formId) + 1);
+      } else {
+        formMap.set(lead.formId, 1);
+      }
+    }
+    this.leadsByForm = [];
+    formMap.forEach((count: number, formId: number) => {
+      const dataPoint = {
+        name: this.formNameMap.get(formId),
+        value: count
+      };
+      this.leadsByForm.push(dataPoint);
+    });
+
+    // fill out the leadsByCampaign object
+    const campaignMap: Map<number, number> = new Map<number, number>();
+    for (const lead of this.leads) {
+      if (campaignMap.has(lead.campaignId)) {
+        campaignMap.set(lead.campaignId, campaignMap.get(lead.campaignId) + 1);
+      } else {
+        campaignMap.set(lead.campaignId, 1);
+      }
+    }
+    this.leadsByCampaign = [];
+    campaignMap.forEach((count: number, campaignId: number) => {
+      const dataPoint = {
+        name: this.campaignNameMap.get(campaignId),
+        value: count
+      };
+      this.leadsByCampaign.push(dataPoint);
     });
   }
 }
